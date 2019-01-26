36ee0802e6f09dd2ccc0653e358b81d5d3623a2a
Vedrana <vedrana_ravlic16@yahoo.com>
2019-01-26 22:29:27 +0100

    created login form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/Login.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Login />
         </header>
       </div>
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 class Login extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <input
+          type="text"
+          ref="username"
+          className="form-control m-3"
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          ref="password"
+          className="form-control m-3"
+          placeholder="Password"
+        />
+        <button
+          //onClick={event => this.handleClick(event)}
+          className="btn btn-primary"
+        >
+          Login
+        </button>
+      </div>
+    );
   }
 }
 export default Login;
