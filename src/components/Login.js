87f9367923739f812733028af7f02e48bbd3e3f9
Vedrana <vedrana_ravlic16@yahoo.com>
2019-01-27 13:13:46 +0100

    posted credentials and gotten token back

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: ""
+    };
+  }
+  handleClick = () => {
+    axios
+      .post("https://www.vumedi.com/api/token-auth/", {
+        username: "frontend@vumedi.com",
+        password: "Frontend123"
+      })
+      //.then(response => response.json())
+      .then(data => this.setState({ token: data.data.token }));
+  };
   render() {
-    // handleClick = () => {
-
-    // }
     return (
       <div className="mx-auto">
         <p className="mx-auto mb-3">Sign in</p>
@@ -20,10 +33,7 @@ class Login extends Component {
           className="form-control mx-auto mb-3"
           placeholder="Password"
         />
-        <button
-          //onClick={this.handleClick()}
-          className="btn btn-primary"
-        >
+        <button onClick={this.handleClick} className="btn btn-primary">
           Login
         </button>
       </div>
